7d8763ecc8cef4b61bd8b9cb2d51a148fb68a337
swarooppatilx <swarooppatil850@gmail.com>
2025-03-13 10:01:18 +0530

    feat: gemini api version update

--- app.py
+++ app.py
@@ -7,8 +7,7 @@ import json
 load_dotenv()
 
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-model = genai.GenerativeModel(model_name="gemini-1.5-flash")
-
+model = genai.GenerativeModel(model_name="gemini-2.0-flash-lite")
 app = Flask(__name__)
 
 @app.route('/')
